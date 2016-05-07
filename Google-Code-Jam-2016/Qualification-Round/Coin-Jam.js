ec5756bac0b76ecfdb9209b706d2fa89a0581e7c
BlackGuyCoding@gmail.com <Blackguycoding@gmail.com>
2016-05-07 23:31:06 +0300

    create file structure

---
+++ Google-Code-Jam-2016/Qualification-Round/Coin-Jam.js
@@ -0,0 +1,5 @@
+/* Google Code Jam 2016 Qualification Round Archived Solutions
+Coin Jam
+Code by [Perrin Clark](BlackGuyCoding@gmail.com)
+Visit the [Google Code Jam](https://code.google.com) page for more information.
+*/
