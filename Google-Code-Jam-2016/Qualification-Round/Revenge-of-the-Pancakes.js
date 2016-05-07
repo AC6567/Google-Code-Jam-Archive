247cfd8adb9584c3a503ad1bff019f5dbdce30c8
BlackGuyCoding@gmail.com <Blackguycoding@gmail.com>
2016-05-07 23:31:17 +0300

    create file structure

---
+++ Google-Code-Jam-2016/Qualification-Round/Revenge-of-the-Pancakes.js
@@ -0,0 +1,5 @@
+/* Google Code Jam 2016 Qualification Round Archived Solutions
+Revenge of the Pancakes
+Code by [Perrin Clark](BlackGuyCoding@gmail.com)
+Visit the [Google Code Jam](https://code.google.com) page for more information.
+*/
